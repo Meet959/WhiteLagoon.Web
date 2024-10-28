00c541709bd9cf45e76b86c6338f8dd78b01ff41
Meet Desai <meetdesai960@gmail.com>
2024-10-28 08:25:04 +0530

    CRUD operations villa model

---
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public VillaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var villas = _context.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa villa)
+        {
+            if(villa.Name == villa.Description)
+            {
+                ModelState.AddModelError("description", "The description cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                villa.Created_Date = DateTime.Now;
+                _context.Villas.Add(villa);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            Villa? villa = _context.Villas.FirstOrDefault(x => x.Id == villaId);
+            if(villa is null)
+            {
+                return NotFound();
+            }
+            return View(villa);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa villa)
+        {
+            if (villa.Name == villa.Description)
+            {
+                ModelState.AddModelError("description", "The description cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid && villa.Id > 0)
+            {
+                var existVilla = _context.Villas.FirstOrDefault(x => x.Id == villa.Id);
+                if (existVilla is not null)
+                {
+                    existVilla.Name = villa.Name;
+                    existVilla.Description = villa.Description;
+                    existVilla.Price = villa.Price;
+                    existVilla.Occupancy = villa.Occupancy;
+                    existVilla.Sqft = villa.Sqft;
+                    existVilla.Updated_Date = DateTime.Now;
+                    _context.Villas.Update(existVilla);
+                    _context.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            return View(villa);
+        }
+
+        public IActionResult Delete(int villaId)
+        {
+            Villa? villa = _context.Villas.FirstOrDefault(x => x.Id == villaId);
+            if (villa is null)
+            {
+                return NotFound();
+            } 
+            return View(villa);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa villa)
+        {
+            Villa existVilla = _context.Villas.FirstOrDefault(x => x.Id == villa.Id);
+            if (existVilla is not null)
+            {
+                _context.Villas.Remove(existVilla);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
